ae10882d7207a6abccbdb49cdb79e5eb5b0ffc89
NgxumzaLG <ngxumzalusanda@gmail.com>
2022-06-09 20:43:25 +0200

    Up to 3-conditional-rendering all

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,56 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isVisible, setIsVisible] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('defualt user');
+
+  useEffect(() => {
+    // PROBLEM FIXED: Asynchronous task in a setTimeout handler
+    // Error occur while I was practicing with useEffect, unfortunately it went south...
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    });
+
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(res.statusText);
+        }
+      })
+      .then((user) => {
+        const { name } = user;
+        setUser(name);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    );
+  }
+
+  return <h2>{user}</h2>;
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,26 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+  // If text is falsey then assign the defualt value 'hello world', but if its truthy then assign the value of text.
+  const firstValue = text || 'hello world';
+  // If text is falsey then by defualt receives an empty string/value, but if its truthy then assign 'hello world'.
+  const secondValue = text && 'hello world';
+  // *****Opposite of the current one = setIsError(!isError)*****NB
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h2>{firstValue}</h2>
+      <h2>Value: {secondValue}</h2>
+      <h2>{text || 'hello world'}</h2>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        Toogle error
+      </button>
+      {isError && <h2 style={{ color: 'red' }}>Error...</h2>}
+      {isError ? <h4>There is an error</h4> : <h4>There is no error</h4>}
+    </>
+  );
 };
 
 export default ShortCircuit;
